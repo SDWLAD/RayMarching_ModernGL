6f22158d9377a8833d2287d01dbfcdf1be5344cf
SDWLAD <sidorchukvladislav0@gmail.com>
2024-10-30 13:34:32 +0200

    First window

---
+++ main.py
@@ -0,0 +1,24 @@
+import moderngl_window as mglw
+
+
+class App(mglw.WindowConfig):
+    window_size = 1200, 800
+    resource_dir = 'shaders'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.quad = mglw.geometry.quad_fs()
+        self.prog = self.load_program(vertex_shader='v.glsl', fragment_shader='f.glsl')
+        self.set_uniform('resolution', self.window_size)
+
+    def set_uniform(self, u_name, u_value):
+        try:
+            self.prog[u_name] = u_value
+        except KeyError:...
+    
+    def render(self, time, frame_time):
+        self.ctx.clear()
+        self.quad.render(self.prog)
+
+if __name__ == '__main__':
+    mglw.run_window_config(App)
