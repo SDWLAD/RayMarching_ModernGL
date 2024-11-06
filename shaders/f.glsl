e9371f6b1cf4c1c1db4b168a8de2910834df8d90
SDWLAD <sidorchukvladislav0@gmail.com>
2024-11-06 21:12:30 +0200

    Added normales.

--- shaders/f.glsl
+++ shaders/f.glsl
@@ -28,17 +28,25 @@ float RayMarch(vec3 ro, vec3 rd){
     return object;
 }
 
+vec3 getNormal(vec3 p) {
+    vec2 e = vec2(0.01, 0.0);
+    vec3 n = vec3(map(p)) - vec3(map(p - e.xyy), map(p - e.yxy), map(p - e.yyx));
+    return normalize(n);
+}
+
 void main(){
     vec2 uv = (2.0 * gl_FragCoord.xy - resolution.xy) / resolution.y;
     vec3 color = vec3(0, 0, 0);
 
-    vec3 ro = vec3(0, 0, -5);
+    vec3 ro = vec3(0, 2, -5);
     vec3 rd = normalize(vec3(uv, 1));
 
     float dist = RayMarch(ro, rd);
+    vec3 p = ro + rd * dist;
+    vec3 n = getNormal(p);
 
     if (dist < 256.0) {
-        color = vec3(1, 1, 1);
+        color = n;
     }
 
     gl_FragColor = vec4(color, 1);
