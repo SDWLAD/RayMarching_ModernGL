4ac4274a43a41e7fa36d824c896e14ad4114a5e5
SDWLAD <sidorchukvladislav0@gmail.com>
2024-12-06 21:35:49 +0200

    Added scene.json

--- scene.py
+++ scene.py
@@ -1,18 +1,32 @@
 from shape import Shape
 import numpy as np
+import json
 
 class Scene:
     def __init__(self, prog):
-        self.shapes = [
-            Shape((0, 3, 0), (1.3,1.3,1.3), (1, 0, 0), 0, 0),
-            Shape((0, 1, 0), (1, 1, 1), (0, 0, 1), 1, 3),
-            Shape((0, 5, 0), (4, 0.6, 0), (0, 1, 0), 2, 0),
-            Shape((0, 0, 0), (0, 0, 0), (1, 1, 1), 3, 0),
-        ]
-        
+        self.load_scene("scene.json")
+
         for i in range(len(self.shapes)):
-            prog[f"shapes[{i}].position"] = self.shapes[i].position
-            prog[f"shapes[{i}].size"] = self.shapes[i].size
-            prog[f"shapes[{i}].color"] = self.shapes[i].color
-            prog[f"shapes[{i}].type"] = self.shapes[i].type
-            prog[f"shapes[{i}].combinationType"] = self.shapes[i].combinationType
+            prog[f"shapes[{i}].position"] = self.shapes[i]["position"]
+            prog[f"shapes[{i}].size"] = self.shapes[i]["size"]
+            prog[f"shapes[{i}].color"] = self.shapes[i]["color"]
+            prog[f"shapes[{i}].type"] = self.encode_types(self.shapes[i]["type"])
+            prog[f"shapes[{i}].combinationType"] = self.encode_types(self.shapes[i]["combinationType"])
+        
+    def load_scene(self, path):
+        with open(path) as f:
+            scene:dict = json.load(f)
+            self.shapes = list(scene.values())
+        print(self.shapes)
+    
+    @staticmethod
+    def encode_types(type):
+        types = {
+            "Sphere":0,
+            "Box":1,
+            "Torus":2,
+            "Plane":3,
+            "Union":0,
+            "SoftUnion":3
+        }
+        return types[type]
